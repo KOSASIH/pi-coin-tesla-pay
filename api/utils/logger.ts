663d1c699b7323fa0714d715263384c4d052bef8
KOSASIH <kosasihg88@gmail.com>
2024-06-09 16:39:58 +0700

    Create logger.ts

---
+++ api/utils/logger.ts
@@ -0,0 +1,12 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+  ],
+});
+
+export default logger;
